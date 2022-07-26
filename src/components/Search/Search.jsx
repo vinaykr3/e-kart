48bc51014b09307ff644247aa644871f0dff8869
VINAY KUMAR PRAJAPATI <vinaykumarprajapati52@gmail.com>
2022-07-26 18:46:38 -0400

    improved search input field

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -4,17 +4,16 @@ import CartIcon from "../Cart/CartIcon"
 import Setting from '../Setting/Setting';
 
 const Search = () => {
+  
+  
   const [show, setShow] = useState(false);
   return (
     <>
       <div className=''>
-        <input type="text" className={`border border-blue-300 px-4 py-1 absolute -bottom-11 ${show ? 'bottom-11 !important' : '-bottom-11'} rounded-md text-gray-900 outline-none w-full  md:hidden transition-all`} placeholder="Search here something..." />
-        <div className='md:w-11/12 mx-auto'>
-          <input type="text" className={`border md:w-11/12 mx-auto border-blue-300 px-2 py-1 hidden md:block absolute top-9 xl:top-11 md:h-10 xl:h-14 rounded-md outline-none md:left-0 md:right-0 transition-all ${show ? 'translate-y-0' : '-translate-y-28'}`} placeholder="Search here something..." />
-        </div>
+        <input type="text" className={`border border-blue-300 px-4 py-1 absolute md:left-0 md:top-0 -bottom-11 md:bottom-auto ${show ? 'bottom-11 !important md:top-1 xl:top-3' : '-bottom-11 md:-top-9 md:bottom-auto'} rounded-md text-gray-900 outline-none w-full  transition-all`} placeholder="Search here something..." />
       </div>
       <div className="search-seting-cart flex justify-between xl:order-2">
-        <AiOutlineSearch className='text-3xl md:text-4xl cursor-pointer mx-4 md:mx-0' onClick={() => { setShow(true) }} />
+        <AiOutlineSearch className='text-3xl md:text-4xl cursor-pointer mx-4 md:mx-0' onClick={() => { !show ? setShow(true) : setShow(false) }} />
         <Setting />
         <CartIcon />
       </div>
